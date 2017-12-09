90debf327a26096f6f32ccd7e03daba7ff7bcd22
studyforgitzxl <1602530367@qq.com>
2017-12-09 14:05:35 +0800

    protected static String TEST="TEST";

--- src/com/bstek/tools/Md5Utils.java
+++ src/com/bstek/tools/Md5Utils.java
@@ -18,8 +18,6 @@ public class Md5Utils {
     protected static char hexDigits[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};  
     protected static MessageDigest messageDigest = null;
     
-    protected static String TEST="TEST";
-    
     static{  
         try{  
             // 拿到一个MD5转换器（如果想要SHA1参数换成”SHA1”）
